b8f70722cd4a9db26fd4574ff2454c044f4a55b6
Liam VINCENT <lixam.vincent@gmail.com>
2023-12-13 01:44:31 +0100

    push

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -2,6 +2,14 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <sys/stat.h>
+
+void replace(std::string filename, std::string s1, std::string s2){
+	std::string newFile = filename + ".replace";
+	
+	std::ofstream outfile(newFile);
+	outfile.close();
+}
 
 int main(int argc, char **argv){	
 	if (argc != 4){
@@ -10,7 +18,7 @@ int main(int argc, char **argv){
 	}
 
 	std::ifstream infile(argv[1]);
-	
+
 	if (infile.fail()){
 		std::cerr << "Error: " << argv[1] << ": File Error" << std::endl; 
 		return (1);
@@ -19,6 +27,6 @@ int main(int argc, char **argv){
 	std::stringstream buffer;
 	buffer << infile.rdbuf();
 	str = buffer.str();
-	std::cout << str;
+	replace(argv[1], argv[2], argv[3]);
 	return (0);
 }
