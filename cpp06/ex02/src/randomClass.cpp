df0173b084421fe34c3dcb8d347585bfd2ab7629
Liam VINCENT <lixam.vincent@gmail.com>
2024-03-11 19:43:24 +0100

    migh have broken my linux install :P

--- cpp06/ex02/src/randomClass.cpp
+++ cpp06/ex02/src/randomClass.cpp
@@ -21,23 +21,18 @@ C::~C() { }
 Base * generate(void)
 {
 	int rand_num = rand() % 3;
-	if (rand_num == 0)
+	switch (rand_num)
 	{
+	case 0:
 		std::cout << "The object type is:		A" << std::endl;
 		return (new A);
-	}
-	else if (rand_num == 1)
-	{
+	case 1:
 		std::cout << "The object type is:		B" << std::endl;
 		return (new B);
-	}
-	else if (rand_num == 2)
-	{
+	case 2:
 		std::cout << "The object type is:		C" << std::endl;
 		return (new C);
-	}
-	else
-	{
+	default:
 		std::cerr << "this shouldn't be possible, what.";
 		exit(1);
 	}
@@ -47,10 +42,15 @@ void identify(Base* p)
 {
 	if (dynamic_cast<A *>(p))
 		std::cout << "The first detected type is:	A" << std::endl;
-	if (dynamic_cast<B *>(p))
+	else if (dynamic_cast<B *>(p))
 		std::cout << "The first detected type is:	B" << std::endl;
-	if (dynamic_cast<C *>(p))
+	else if (dynamic_cast<C *>(p))
 		std::cout << "The first detected type is:	C" << std::endl;
+	else
+	{
+		std::cerr << "what." << std::endl;
+		exit(1);
+	}
 }
 
 void identify(Base& p)
@@ -61,19 +61,19 @@ void identify(Base& p)
 		std::cout << "The second detected type is:	A" << std::endl;
 		return ;
 	}
-	catch ( ... ) {}
+	catch ( ... ) { }
 	try 
 	{
 		B temp = dynamic_cast<B &>(p);
 		std::cout << "The second detected type is:	B" << std::endl;
 		return ;
 	}
-	catch ( ... ) {}
+	catch ( ... ) { }
 	try
 	{
 		C temp = dynamic_cast<C &>(p);
 		std::cout << "The second detected type is:	C" << std::endl;
 		return ;
 	}
-	catch ( ... ) {}
+	catch ( ... ) { }
 }
