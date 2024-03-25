85cc33d749a9c26c20baa45df22bdfaad19469eb
Liam VINCENT <lixam.vincent@gmail.com>
2024-03-25 11:50:31 +0100

    stuff

---
+++ cpp09/ex00/src/main.cpp
@@ -0,0 +1,16 @@
+#include "BitcoinExchange.hpp"
+#include <iostream>
+
+int main(int argc, char argv[])
+{
+	if (argc < 2)
+	{
+		std::cerr << "You must give a file as an argument" << std::endl;
+		return (1);
+	}
+	if (argc > 2)
+	{
+		std::cerr << "Too many arguments" << std::endl;
+		return (1);
+	}
+}
