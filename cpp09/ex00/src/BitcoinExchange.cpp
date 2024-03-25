f92bc4d18b3de9d8c2ea096de958c654ef17c998
Liam Vincent <lixam.vincent@gmail.com>
2024-03-25 20:00:00 +0100

    caca

---
+++ cpp09/ex00/src/BitcoinExchange.cpp
@@ -0,0 +1,8 @@
+#include "BitcoinExchange.hpp"
+
+#include <string>
+
+void BitcoinExchange::initData(void)
+{
+
+}
---
+++ cpp09/ex00/src/BitcoinExchange.hpp
@@ -0,0 +1,24 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+#include <map>
+#include <string>
+
+class BitcoinExchange
+{
+	private:
+		std::map<std::string, float> database;
+		std::string filename;
+
+		void initData(void);
+		void parseLine(std::string line);
+
+	public:
+		BitcoinExchange(void);
+		~BitcoinExchange(void);
+		BitcoinExchange(std::string filename);
+		BitcoinExchange(const BitcoinExchange& copy);
+		BitcoinExchange& operator=(const BitcoinExchange& copy);
+};
+
+#endif
